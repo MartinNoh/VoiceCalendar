1d57258acda8345ac48abde11accc756275bc536
Donggyeong <ehdrud1129@naver.com>
2020-05-27 04:55:18 +0900

    VoiceCalendar design material_calendar_view ver6 commit

--- app/src/main/java/com/androidcode/voicecalendar/decorators/EventDecorator.java
+++ app/src/main/java/com/androidcode/voicecalendar/decorators/EventDecorator.java
@@ -7,6 +7,7 @@ import com.androidcode.voicecalendar.R;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -34,6 +35,6 @@ public class EventDecorator implements DayViewDecorator {
     @Override
     public void decorate(DayViewFacade view) {
         view.setSelectionDrawable(drawable);
-        //view.addSpan(new DotSpan(5, color)); // 날자밑에 점
+        view.addSpan(new DotSpan(5, color)); // 날자밑에 점
     }
 }
